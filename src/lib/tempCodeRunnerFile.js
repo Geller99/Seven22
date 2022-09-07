9d9821c48cb5006440666576febefa5f2e2e1e0b
geller99 <gellera800@gmail.com>
2022-09-07 14:19:17 -0400

    Updated Generator

--- src/lib/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-
-
-// const leafNodes = allowList.map(address => keccak256(address));
-// const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-// const rootHash = merkleTree.getRoot();
